7fc25e1707e8228d2bfd4bef00ef00c4290b810a
Bacon004 <jacobsuid@yahoo.com>
2019-10-11 12:55:33 -0400

    added bot login

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,24 @@
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import javax.security.auth.login.LoginException;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        final Properties properties = new Properties();
-        properties.load(new FileInputStream("bot.properties"));
-        System.out.println(properties.get("token"));
+public class Main extends ListenerAdapter {
+    public static void main(String[] args) {
+        try {
+            final Properties properties = new Properties();
+            properties.load(new FileInputStream("bot.properties"));
+            new JDABuilder(AccountType.BOT)
+                    .setToken(String.valueOf(properties.get("token")))
+                    .buildAsync();
+        } catch(IOException | LoginException ex) {
+            ex.printStackTrace();
+        }
     }
+
+
 }
