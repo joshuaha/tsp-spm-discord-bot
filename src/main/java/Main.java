8d1869987064d4fb5e65673fc6acc475d3dfb2d5
Bacon004 <jacobsuid@yahoo.com>
2019-10-11 12:46:19 -0400

    added token properties file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        final Properties properties = new Properties();
+        properties.load(new FileInputStream("bot.properties"));
+        System.out.println(properties.get("token"));
     }
 }
