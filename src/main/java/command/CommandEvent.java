d72dbd0768fe0b6d2f67b0b870de6a8da6d56a47
Nels Raisanen <nelsraisanen@hotmail.com>
2019-10-23 12:47:34 -0400

    Update implementation of edit poll and event

--- src/main/java/command/CommandEvent.java
+++ src/main/java/command/CommandEvent.java
@@ -20,6 +20,67 @@ public class CommandEvent implements Command {
      */
     @Override
     public void execute(String[] args, MessageReceivedEvent event) {
+        if ( "create".equals( args[0] ) ) {
+            final String eventName = args[1];
+            final String eventLoc = args[2];
+            final String eventTime = args[3];
+            this.create( eventName, eventLoc, eventTime );
+
+        } else if ( "answer".equals( args[0] ) ) {
+            final String eventName = args[1];
+            final String going = args[2];
+            boolean isGoing = false;
+            if ( going.equals( "y" ) ) {
+                isGoing = true;
+            } else if ( going.equals( "n" ) ) {
+                isGoing = false;
+            }
+            final long user = event.getAuthor().getIdLong();
+            this.answer( eventName, isGoing, user, event );
+
+        } else if ("results".equals(args[0])) {
+
+            final String eventName = args[1];
+            this.results( eventName, event );
+
+        } else if ( "edit".equals( args[0] ) ) {
+            final String eventName = args[1];
+            final String edit = args[2];
+            if ( "name".equalsIgnoreCase( edit ) ) {
+
+            } else if ( "location".equalsIgnoreCase( edit ) ) {
+
+            } else if ( "time".equalsIgnoreCase( edit ) ) {
+
+            }
+
+        } else {
+            //TODO - Send message saying invalid command and give them help.
+            return;
+        }
+    }
+
+    private void create ( String name, String location, String time ) {
+
+    }
+
+    private void results ( String name, MessageReceivedEvent event ) {
+
+    }
+
+    private void editName( String name, String newName ) {
+
+    }
+
+    private void editLocation ( String name, String newLocation ) {
+
+    }
+
+    private void editTime ( String name, String newTime ) {
+
+    }
+
+    private void answer ( String name, boolean isGoing, long user, MessageReceivedEvent event ) {
 
     }
 }
--- src/main/java/command/CommandPoll.java
+++ src/main/java/command/CommandPoll.java
@@ -4,8 +4,11 @@ import factory.DaoFactory;
 import poll.DiscordPoll;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import poll.DiscordPollDao;
+import poll.DiscordPollDaoSql;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class CommandPoll implements Command {
     private final DiscordPollDao pollDao  = DaoFactory.getDiscordPollDao();
@@ -28,17 +31,69 @@ public class CommandPoll implements Command {
     @Override
     public void execute(String[] args, MessageReceivedEvent event) {
         if ("create".equals(args[0])) {
+
             final String pollName = args[1];
             final String[] options = Arrays.copyOfRange(args, 2, args.length);
             this.create(pollName, options);
+
         } else if ("vote".equals(args[0])) {
+
             final String pollName = args[1];
             final int vote = Integer.parseInt(args[2]) - 1;
             final long user = event.getAuthor().getIdLong();
             this.vote(pollName, user, vote, event);
+            //TODO - Let user know vote is index not name of option.
+
         } else if ("results".equals(args[0])) {
+
             final String pollName = args[1];
             this.results(pollName, event);
+
+        } else if ( "edit".equals( args[0] ) ) {
+
+            final String pollName = args[1];
+            final String edit = args[2];
+            if ( "name".equalsIgnoreCase( edit ) ) {
+
+                this.pollDao.getPoll( pollName ).setName( args[3] );
+
+            } else if ( "option".equalsIgnoreCase( edit ) ) {
+
+                int optionIndex = Integer.parseInt( args[3] );
+                final String newOption = args[4];
+
+                if ( optionIndex > this.pollDao.getPoll(pollName).getOptions().size() ) {
+
+                    if ( optionIndex > Integer.MAX_VALUE ) optionIndex = Integer.MAX_VALUE;
+                    if ( optionIndex <= 0 ) {
+                        final StringBuilder message = new StringBuilder();
+                        message.append( "Poll indexes begin at 1" );
+                        event.getMessage().getAuthor().openPrivateChannel().queue((channel)
+                                -> channel.sendMessage(message).queue());
+                        return;
+                    }
+
+                    List<DiscordPoll.Option> options = this.pollDao.getPoll( pollName ).getOptions();
+                    List<String> newOptions = new ArrayList<>();
+                    for (DiscordPoll.Option o : options ) {
+                        newOptions.add( o.getText() );
+                    }
+                    newOptions.add( newOption );
+
+                    this.pollDao.getPoll( pollName ).setOptions( newOptions );
+
+                    event.getChannel().sendMessage( this.pollDao.getPoll( pollName).getName()
+                            + "'s options were updated." );
+                }
+
+                //TODO - Edit existing option.
+                //TODO - Reset results and notify that poll results are reset for X Option.
+
+            }
+
+        } else {
+            //TODO - Send message saying invalid command and give them help.
+            return;
         }
     }
 
