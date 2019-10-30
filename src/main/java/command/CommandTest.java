df5cb8ac6ac93da5c826cfdcf1f75462eccf1408
Nels Raisanen <nelsraisanen@hotmail.com>
2019-10-30 12:18:01 -0400

    Ignore automatic check for test

--- src/main/java/command/CommandTest.java
+++ src/main/java/command/CommandTest.java
@@ -4,6 +4,7 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import test.TestingBot;
 
 public class CommandTest implements Command {
+
     TestingBot tb;
     CommandTest() {
         tb = new TestingBot();
@@ -17,7 +18,10 @@ public class CommandTest implements Command {
     @Override
     public void execute(String[] args, MessageReceivedEvent event) {
         tb.sendTestStrings( event );
-        if ( tb.checkOutput( event ) ) System.out.println( "Test passed!" );
-        else System.out.println( "Test failed.");
+
+        //Ignoring check output more work to automate than to manually check output.
+//        if ( tb.checkOutput( event ) ) System.out.println( "Test passed!" );
+//        else System.out.println( "Test failed.");
     }
+
 }
