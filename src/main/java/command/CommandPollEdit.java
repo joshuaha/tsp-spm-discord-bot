d5372f6f82fce66e3640dbeaa4b3f1b06cf275b7
Bacon004 <jacobsuid@yahoo.com>
2019-11-11 12:57:12 -0500

    added formatting for date time display strings and inputs

--- src/main/java/command/CommandPollEdit.java
+++ src/main/java/command/CommandPollEdit.java
@@ -3,6 +3,8 @@ package command;
 import factory.ServiceFactory;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import org.joda.time.LocalDateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import poll.DiscordPoll;
 import poll.DiscordPollDao;
 
@@ -10,6 +12,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CommandPollEdit implements Command {
+    private static final DateTimeFormatter DATE_OUTPUT_FORMAT = DateTimeFormat.forPattern("M-d-yyyy");
+    private static final DateTimeFormatter TIME_OUTPUT_FORMAT = DateTimeFormat.forPattern("h:mma");
+    private static final DateTimeFormatter DATE_TIME_INPUT_FORMAT = DateTimeFormat.forPattern("M-d-yy h:mma");
     private final DiscordPollDao pollDao = ServiceFactory.getDiscordPollDao();
 
     /**
@@ -26,7 +31,6 @@ public class CommandPollEdit implements Command {
     @Override
     public void execute(String[] args, MessageReceivedEvent event) {
         final DiscordPoll poll = pollDao.getPoll(args[0]);
-        System.out.println(poll.getOpenTime());
         if (event.getAuthor().getIdLong() == poll.getOwnerId()) {
             //get the property the user wishes to edit
             final String property = args[1];
@@ -56,17 +60,22 @@ public class CommandPollEdit implements Command {
                     //report if the provided index was invalid
                     event.getChannel().sendMessage(String.format("%d is not a valid index.", index + 1)).queue();
                 }
-            }
-            else if("opentime".equalsIgnoreCase(property)) {
+            } else if ("opentime".equalsIgnoreCase(property)) {
                 //update the poll open time
-                poll.setOpenTime(new LocalDateTime(args[2]));
+                final LocalDateTime openTime = LocalDateTime.parse(args[2], DATE_TIME_INPUT_FORMAT);
+                poll.setOpenTime(openTime);
                 this.pollDao.updatePoll(poll);
-                event.getChannel().sendMessage("Poll open time successfully update. Poll now begins at " + poll.getOpenTime()).queue();
-            } else if("closetime".equalsIgnoreCase(property)) {
+                final String openDateString = poll.getOpenTime().toString(DATE_OUTPUT_FORMAT);
+                final String openTimeString = poll.getOpenTime().toString(TIME_OUTPUT_FORMAT);
+                event.getChannel().sendMessage(String.format("Poll open time successfully update. Poll now begins on %s at %s.", openDateString, openTimeString)).queue();
+            } else if ("closetime".equalsIgnoreCase(property)) {
                 //update the poll close time
-                poll.setCloseTime(new LocalDateTime(args[2]));
+                final LocalDateTime closeTime = LocalDateTime.parse(args[2], DATE_TIME_INPUT_FORMAT);
+                poll.setCloseTime(closeTime);
                 this.pollDao.updatePoll(poll);
-                event.getChannel().sendMessage("Poll end time successfully update. Poll now ends at " + poll.getCloseTime()).queue();
+                final String closeDateString = poll.getCloseTime().toString(DATE_OUTPUT_FORMAT);
+                final String closeTimeString = poll.getCloseTime().toString(TIME_OUTPUT_FORMAT);
+                event.getChannel().sendMessage(String.format("Poll end time successfully update. Poll now ends on %s at %s", closeDateString, closeTimeString)).queue();
             }
             //update poll message
             final List<String> options = this.pollDao.getOptions(poll.getId());
